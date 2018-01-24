1e22b258466e4c3df0751d766ad42c0342db7fa5
Ana Ortega <anaa0hh@icloud.com>
2018-01-23 19:50:25 -0800

    more organized version of sales table/includes hours of day

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 'use strict';
+
+var firstAndPike = {
+  min: 23,
+  max: 65,
+  checkAverage: function() {
+    return (this.min + this.max) / 2;
+  }
+};
+
+console.log(firstAndPike.checkAverage);
+
+var seatacAirport = {
+  min: 23,
+  max: 65,
+  checkAverage: function() {
+    return (this.min + this.max) / 2;
+  }
+};
+
+console.log(seatacAirport.checkAverage);
+
+var seattleCenter = {
+  min: 23,
+  max: 65,
+  checkAverage: function() {
+    return (this.min + this.max) / 2;
+  }
+};
+
+console.log(seattleCenter.checkAverage);
+
+var capitolHill = {
+  min: 23,
+  max: 65,
+  checkAverage: function() {
+    return (this.min + this.max) / 2;
+  }
+};
+
+console.log(capitolHill.checkAverage);
+
+var alki = {
+  min: 23,
+  max: 65,
+  checkAverage: function() {
+    return (this.min + this.max) / 2;
+  }
+};
+  
+console.log(alki.checkAverage);
