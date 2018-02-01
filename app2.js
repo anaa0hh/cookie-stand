c45f846b709c84c97af99841d1a10a3ade93940e
Ana Ortega <anaa0hh@icloud.com>
2018-02-01 15:16:42 -0800

    linked correct js page

--- app2.js
+++ app2.js
@@ -1,21 +1,5 @@
 'use strict';
-
-var hours = [
-  '6am',
-  '7am',
-  '8am',
-  '9am',
-  '10am',
-  '11am',
-  '12pm',
-  '1pm',
-  '2pm',
-  '3pm',
-  '4pm',
-  '5pm',
-  '6pm',
-  '7pm'
-];
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
 var allLocations = [];
 var totalCookiesByHour = 0;
@@ -26,50 +10,34 @@ function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour
   this.name = name;
   this.minCustPerHour = minCustPerHour;
   this.maxCustPerHour = maxCustPerHour;
-  this.avgCookieSoldPerHour =avgCookieSoldPerHour;
+  this.avgCookieSoldPerHour = avgCookieSoldPerHour;
   this.randCustByHour = [];
-  this.avgCookieSoldByHour = [];
+  this.cookiesSoldByHour = [];
   this.totalCookies = 0;
   allLocations.push(this);
-
-
-
   this.calcRandCustByHour = function() {
     for (var i = 0; i < hours.length; i++) {
-      this.randCustByHour.push(
-        Math.floor(
-          Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)
-        ) + this.minCustPerHour
-      );
+      this.randCustByHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
       console.log(this.randCustByHour[i]);
-
-    }
-  };
-
-
-
-  this.calcCookiesSoldByHour = function() {
-    for (var j = 0; j < hours.length; j++) {
-      this.cookiesSoldByHour.push(
-        Math.round(this.avgCookiesSoldPerHour * this.randCustByHour[j])
-      );
-      console.log(this.cookiesSoldByHour[j]);
     }
 
+    this.calcCookiesSoldByHour = function() {
+      for (var i = 0; i < hours.length; i++) {
+        this.cookiesSoldByHour.push(Math.ceil(this.avgCookiesSoldPerHour * this.randCustByHour[i]));
+        console.log(this.cookiesSoldByHour[i]);
+        this.totalCookies += this.avgCookieSoldPerHour[i];
+      }
+    };
+    this.calcRandCustByHour();
+    this.calcCookiesSoldByHour();
   };
-
 }
-
-
-
+// call constructors
 function makeStands() {
   new MakeLocation('First and Pike', 23, 65, 6.3);
   new MakeLocation('SeaTac Airport', 3, 24, 1.2);
   new MakeLocation('Seattle Center', 11, 38, 3.7);
   new MakeLocation('Capitol Hill', 20,38, 2.3);
   new MakeLocation('Alki', 2, 16, 4.6);
-
 }
-console.log(allLocations);
 makeStands();
-
